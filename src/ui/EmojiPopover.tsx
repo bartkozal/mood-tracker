a88c94bddf7e634b104ba16bf35ea0238bd6c67d
Bart Kozal <brtjkzl@gmail.com>
2019-02-18 00:28:12 +0100

    Add emojis to popover

--- src/ui/EmojiPopover.tsx
+++ src/ui/EmojiPopover.tsx
@@ -1,6 +1,12 @@
 import React from "react";
-import { Text, TouchableWithoutFeedback, StyleSheet } from "react-native";
+import {
+  TouchableWithoutFeedback,
+  StyleSheet,
+  View,
+  Image
+} from "react-native";
 import Popover from "react-native-popover-view";
+import Color from "./Color";
 
 interface Props {
   isVisible: boolean;
@@ -17,21 +23,92 @@ export default class EmojiPopover extends React.Component<Props> {
         isVisible={isVisible}
         fromView={fromView}
         onClose={onClose}
-        placement="bottom"
+        placement="auto"
         showBackground={false}
         popoverStyle={styles.popover}
         animationConfig={{ duration: 100 }}
       >
-        <Text>😀🙃😎</Text>
+        <View style={styles.emojiRowView}>
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/grin.png")}
+          />
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/smile.png")}
+          />
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/neutral.png")}
+          />
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/sad.png")}
+          />
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/cry.png")}
+          />
+        </View>
+        <View style={styles.emojiRowView}>
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/sleeping.png")}
+          />
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/love.png")}
+          />
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/zany.png")}
+          />
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/party.png")}
+          />
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/cool.png")}
+          />
+        </View>
+        <View style={styles.emojiRowView}>
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/scream.png")}
+          />
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/sick.png")}
+          />
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/what.png")}
+          />
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/angry.png")}
+          />
+          <Image
+            style={styles.emoji}
+            source={require("../../assets/emojis/rage.png")}
+          />
+        </View>
       </Popover>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  emojiRowView: {
+    flexDirection: "row"
+  },
+  emoji: {
+    margin: 8
+  },
   popover: {
     padding: 8,
-    backgroundColor: "red",
+    backgroundColor: Color.Sky,
     shadowColor: "rgba(0, 0, 0, 0.3)",
     shadowOffset: {
       width: 0,
