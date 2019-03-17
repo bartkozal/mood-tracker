7116426b077e2289da26da75aac793fd44a9675c
Bart Kozal <bart@eclecticiq.com>
2019-03-17 19:13:36 +0100

    Create custom tab navigation

--- src/app/TabNavigator.tsx
+++ src/app/TabNavigator.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { TouchableOpacity } from "react-native";
 import { createBottomTabNavigator } from "react-navigation";
 import MainScreen from "../screens/MainScreen";
 import AnalyticsScreen from "../screens/AnalyticsScreen";
@@ -27,6 +28,24 @@ export default createBottomTabNavigator(
   },
   {
     defaultNavigationOptions: ({ navigation }) => ({
+      tabBarButtonComponent: (props: any) => {
+        const isActive = props.children[0].props.activeOpacity === 1;
+
+        return (
+          <TouchableOpacity
+            {...props}
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "flex-end",
+              flexDirection: "column",
+              paddingTop: isActive ? 12 : 15,
+              borderTopColor: Color.Sky,
+              borderTopWidth: isActive ? 3 : 0
+            }}
+          />
+        );
+      },
       tabBarIcon: ({ focused }) => {
         const name = getIconName(navigation.state.routeName);
         const color = focused ? "sky" : "gray";
@@ -37,9 +56,8 @@ export default createBottomTabNavigator(
     tabBarOptions: {
       showLabel: false,
       style: {
-        paddingTop: 16,
-        borderTopColor: Color.Sky,
-        borderTopWidth: 3
+        paddingTop: 0,
+        borderTopWidth: 0
       },
       activeTintColor: Color.Sky,
       inactiveTintColor: Color.Gray
