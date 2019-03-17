641f34ef3b7872f3d9ffbb1d4951ecf9e202b04f
Bart Kozal <bart@eclecticiq.com>
2019-03-17 14:09:59 +0100

    Fix offset calculation

--- src/ui/Calendar/index.tsx
+++ src/ui/Calendar/index.tsx
@@ -15,11 +15,18 @@ interface Props {
   onDayMoodChange: (day: string, mood: string) => void;
 }
 
+interface MonthItem {
+  month: number;
+  monthName: string;
+  numberOfDays: number;
+  firstWeekday: number;
+}
+
 const HEADER_HEIGHT = 70.33333587646484;
 const FIVE_ROWS_MONTH_HEIGHT = 348.666748046875;
 const SIX_ROWS_MONTH_HEIGHT = 397.666748046875;
 
-const calculateMonthHeight = (firstWeekday: number, numberOfDays: number) => {
+const calculateMonthHeight = ({ firstWeekday, numberOfDays }: MonthItem) => {
   const numberOfAvailableSlotsInFiveRowsMonth = 35;
   const numberOfTakenSlots = firstWeekday - 1 + numberOfDays;
   const isFiveRowsMonth =
@@ -28,11 +35,26 @@ const calculateMonthHeight = (firstWeekday: number, numberOfDays: number) => {
   return isFiveRowsMonth ? FIVE_ROWS_MONTH_HEIGHT : SIX_ROWS_MONTH_HEIGHT;
 };
 
-const getViewOffset = (month: number) => (month === 1 ? 0 : -HEADER_HEIGHT);
-const getViewPosition = (month: number) => 0;
+const calculateMonthOffset = ({ month }: MonthItem, months: MonthItem[]) => {
+  if (month === 1) return 0;
+
+  return months.reduce((offset, item) => {
+    if (item.month >= month) return offset;
+
+    return offset + calculateMonthHeight(item);
+  }, 0);
+};
+
+const getViewOffset = (month: number) => {
+  if (month === 1) return 0;
+  if (month === 12) return HEADER_HEIGHT;
+  return -HEADER_HEIGHT;
+};
+
+const getViewPosition = (month: number) => (month === 12 ? 1 : -0.5);
 
 export default class Calendar extends React.Component<Props> {
-  flatList = React.createRef<FlatList<any>>();
+  flatList = React.createRef<FlatList<MonthItem>>();
 
   componentDidMount() {
     const { activeMonth } = this.props;
@@ -51,7 +73,7 @@ export default class Calendar extends React.Component<Props> {
   render() {
     const { activeYear, onDayMoodChange, savedMood } = this.props;
     const monthNames = Info.months("short");
-    const months = monthNames.map(monthName => {
+    const months: MonthItem[] = monthNames.map(monthName => {
       const month = monthNames.indexOf(monthName) + 1;
       const monthDateTime = DateTime.fromObject({
         year: activeYear,
@@ -73,12 +95,13 @@ export default class Calendar extends React.Component<Props> {
         keyExtractor={item => String(item.month)}
         showsVerticalScrollIndicator={false}
         getItemLayout={(months, index) => {
-          const { firstWeekday, numberOfDays } = months![index];
-          const monthHeight = calculateMonthHeight(firstWeekday, numberOfDays);
+          const month = months![index];
+          const length = calculateMonthHeight(month);
+          const offset = calculateMonthOffset(month, months!);
 
           return {
-            length: monthHeight,
-            offset: monthHeight * index, // TODO doesn't include 6 row months
+            length,
+            offset,
             index
           };
         }}
