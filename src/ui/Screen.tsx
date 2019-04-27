1203b5a25e95dfbef41cf5d4314e330bbce52f65
Bart Kozal <brtjkzl@gmail.com>
2019-04-27 23:39:04 +0200

    Fix screen padding for Android

--- src/ui/Screen.tsx
+++ src/ui/Screen.tsx
@@ -1,19 +1,28 @@
 import React from "react";
+import { Platform, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-navigation";
-import { LinearGradient } from "expo";
+import { LinearGradient, Constants } from "expo";
 import { Gradient } from "./Color";
 
 export default class Screen extends React.Component {
   render() {
     return (
       <SafeAreaView>
-        <LinearGradient
-          colors={Gradient.Background}
-          style={{ paddingHorizontal: 8 }}
-        >
+        <LinearGradient colors={Gradient.Background} style={styles.screenView}>
           {this.props.children}
         </LinearGradient>
       </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  screenView: {
+    paddingHorizontal: 8,
+    ...Platform.select({
+      android: {
+        paddingTop: Constants.statusBarHeight
+      }
+    })
+  }
+});
